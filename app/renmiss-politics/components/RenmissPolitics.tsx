b701534be90a9d6d6b40d19c54ebf2d3dc46b1b5
Toluhi Kay <toluhikay@users.noreply.github.com>
2023-07-12 11:05:47 +0100

    repeaters page added

--- app/renmiss-politics/components/RenmissPolitics.tsx
+++ app/renmiss-politics/components/RenmissPolitics.tsx
@@ -4,7 +4,7 @@ const RenmissPolitics = () => {
   return (
     <div className="flex bg-[#0C240B] md:mt-[110px] mt-[70px] lg:py-[136px] md:py-[90px] py-[70px] px-[15px] flex-col items-center">
       <div className="flex items-center md:text-5xl text-2xl font-bold mb-[67px]">
-        <h1 className="mr-2 text-white">Renmiss</h1> <div className="bg-about-white-mobile bg-cover  flex items-center justify-center p-4 w-[250px] min-h-[90px] text-white"> Politics</div>
+        <h1 className="mr-2 text-white">Renmiss</h1> <div className="bg-about-white-mobile bg-cover  flex items-center justify-center p-4 md:w-[250px] w-[200px] min-h-[70px]  md:min-h-[90px] text-white"> Politics</div>
       </div>
       <p className="text-center tracking-wider md:text-2xl text-base sm:w-[90%] text-[#FFFFFF]">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed d Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dLorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a,
